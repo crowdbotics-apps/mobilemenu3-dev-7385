2b6cde816f9d7aee6f770b4596de5a591029ebd3
Crowdbotics <team@crowdbotics.com>
2020-07-10 14:59:36 +0000

    Initial commit.

---
+++ ios/mobilemenu3_dev_7385Tests/mobilemenu3_dev_7385Tests.m
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
+
+@interface mobilemenu3_dev_7385 : XCTestCase
+
+@end
+
+@implementation mobilemenu3_dev_7385
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
